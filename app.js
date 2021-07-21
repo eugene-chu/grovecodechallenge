8692b83c5f6f0563ff85f0930edd3d3f2a0081fb
Eugene Chu <eugenchu@gmail.com>
2021-07-20 23:36:41 -0700

    Clarity variable name in main, rename getData to getOrder for clarity, extract the API call into its own function since it will be used in getCity. Create a getCity function to get the city/cities for their tax rate

--- app.js
+++ app.js
@@ -12,29 +12,39 @@
 const axios = require('axios');
 
 async function main () {
-  let data = await getData();
-  data.forEach(order => {
+  let orders = await getOrder();
+  if(orders === null){
+    console.error('There was an error retrieving orders from API');
+    return;
+  }
+  orders.forEach(order => {
     console.log(`Order: ${order.id}\nCustomer Name: ${order.shipping_name}`);
+    debugger;
   });
 }
 
 main();
 
-async function getData (id = undefined) {
+async function getOrder(id = undefined) {
   let APIurl = 'https://code-challenge-i2hz6ik37a-uc.a.run.app/orders';
-  if(typeof id !== 'undefined'){
-    APIurl += `/${id}`;
-  }
-  let data;
-  await axios.get(APIurl).then(res => {
-    if(res.status !== 200){
-      console.log('Status Code response 400');
-      return data = null;
-    }
-    data = res.data;
-  }).catch(err => {
-    console.log(`Error occur when calling the API. There error is: ${err}`)
-    return data = null;
-  })
-  return data;
+
+  return await getData(APIurl, id);
 };
+
+async function getCity(zip = undefined) {
+  let APIurl = 'https://code-challenge-i2hz6ik37a-uc.a.run.app/cities';
+
+  return await getData(APIurl, zip);
+};
+
+async function getData(APIurl, param){
+  if(typeof para !== 'undefined') APIurl += `/${param}`;
+  let returndata;
+  await axios.get(APIurl)
+    .then(res => res.status === 200 ? returndata = res.data : returndata = null)
+    .catch(err => {
+      console.error(`Error occured when calling the API. The error is: ${err}`);
+      returndata = null;
+  })
+  return returndata;
+}
