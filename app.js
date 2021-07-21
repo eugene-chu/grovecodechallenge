b6f381c261ae62c909ffff0449e65c5bb1cd7bff
Eugene Chu <eugenchu@gmail.com>
2021-07-20 20:50:02 -0700

    App grabs all order data from the API endpoint. Currently having it print all the order ids and customer names

--- app.js
+++ app.js
@@ -8,3 +8,37 @@
 // -- exit
 // Continue to prompt until exit
 // exit
+
+const { promisify } = require('util');
+const axios = require('axios');
+
+const getData = async (id = undefined) => {
+  let APIurl = 'https://code-challenge-i2hz6ik37a-uc.a.run.app/orders';
+  if(typeof id !== 'undefined'){
+    APIurl += `/${id}`;
+  }
+  let data;
+  await axios.get(APIurl).then(res => {
+    // console.log(res);
+    if(res.status !== 200){
+      console.log('Status Code response 400');
+      return data = null;
+    }
+    return data = res.data;
+  }).catch(err => {
+    console.log(`Error occur when calling the API. There error is: ${err}`)
+    return data = null;
+
+  })
+  return data;
+};
+
+const main = async () => {
+  let data = await getData();
+  // console.log(data);
+  data.forEach(order => {
+    console.log(`Order: ${order.id}\nCustomer Name: ${order.shipping_name}`);
+  });
+}
+
+main();
